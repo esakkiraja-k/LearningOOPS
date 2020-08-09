8e05d154be0b8c710f4247c727cc52dbcc72d5e5
EKandas1 <esakkiraja_kandasamy@mcafee.com>
2020-08-09 20:00:55 +0530

    Value Objects

---
+++ ImmutableObjects/MoneyAmount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ImmutableObjects
+{
+   sealed class MoneyAmount :IEquatable<MoneyAmount>
+    {
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            this.Amount = amount;
+            this.CurrencySymbol = currencySymbol;
+
+        }
+        public decimal Amount { get;  }
+        public string CurrencySymbol { get; }
+
+        public MoneyAmount Scale(decimal factor) =>
+            new MoneyAmount(this.Amount * factor, "USD");
+        public override string ToString()
+        {
+            return $"{this.Amount} {this.CurrencySymbol}";
+        }
+
+        public override bool Equals(object obj) => this.Equals(obj as MoneyAmount);
+
+        public override int GetHashCode() => this.Amount.GetHashCode() ^ this.CurrencySymbol.GetHashCode();
+        public bool Equals(MoneyAmount other) =>
+            other != null &&
+            this.Amount == other.Amount &&
+            this.CurrencySymbol == other.CurrencySymbol;
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b) =>
+            (Object.ReferenceEquals(a,null) && Object.ReferenceEquals(b,null)) || 
+            (!Object.ReferenceEquals(a,null) && a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b) => !(a == b);
+
+
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factor) => amount.Scale(factor);
+    }
+}
---
+++ ImmutableObjects/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImmutableObjects
+{
+    class Program
+    {
+        static bool IsHappyHour { get; set; }
+
+
+
+        static MoneyAmount Reserve(MoneyAmount cost)
+        {
+            decimal factor = 1;
+            if (IsHappyHour)
+            {
+                factor =  .5M;
+            }
+            Console.WriteLine("\n reserving the item costs {0}", cost);
+            return cost.Scale(factor);
+        }
+        static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            bool enoughMoney = wallet.Amount > cost.Amount;
+
+            MoneyAmount finalCost = Reserve(cost);
+
+            bool finalEnough = wallet.Amount > finalCost.Amount;
+
+            if (enoughMoney && finalEnough)
+                Console.WriteLine("you will pay {0} from your {1}",cost,wallet);
+            else if(finalEnough)
+                Console.WriteLine("this time, {0} will be enough to pay {1}", wallet,finalCost);
+            else
+                Console.WriteLine("you cannot pay {0} from your {1}", cost, wallet);
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Buy(new MoneyAmount (12,  "USD"),
+                new MoneyAmount (10, "USD"));
+
+            Buy(new MoneyAmount (7,  "USD"),
+                new MoneyAmount (10,"USD"));
+
+            IsHappyHour = true;
+            Buy(new MoneyAmount (7, "USD" ),
+                new MoneyAmount (10, "USD" ));
+
+            //Equality check
+            MoneyAmount x = new MoneyAmount(10, "USD");
+            MoneyAmount Y = new MoneyAmount(10, "USD");
+
+            if(x.Equals(Y))
+                Console.WriteLine("X and Y are equal");
+
+
+            //Hashcode override
+            MoneyAmount a = new MoneyAmount(10, "USD");
+            MoneyAmount b = new MoneyAmount(10, "USD");
+            
+            HashSet<MoneyAmount> sets = new HashSet<MoneyAmount>();
+
+            sets.Add(a);
+            if (sets.Contains(b))
+                Console.WriteLine("already exist cannot be repeated");
+            else
+                sets.Add(b); // default Gethashcode impl returns value based on object reference
+
+            Console.WriteLine("Hashset count {0}",sets.Count);
+
+
+
+            //Custom Equality check
+            MoneyAmount c = new MoneyAmount(10, "USD");
+            MoneyAmount d = new MoneyAmount(10, "USD");
+
+            if (c == d)
+                Console.WriteLine("X and Y are equal");
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
